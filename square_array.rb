a49fdad50293a7b800684f2b65ee06c0136cdf33
Bryant Omoregie <unclebryant@gmail.com>
2020-01-28 21:13:44 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,14 +1,7 @@
-#build method square_array
-#use each as iterator 
-#take an array of numbers
-#square each numbers
-#returns a ****new**** array of the squared numbers
-
-
-
 def square_array(array)
-  arr = []
-  
-  
-new_array(array)
+  arr = [ ]
+  arr.each do | i |
+    new_array << i ** 2
+end
+return new_array
 end 
